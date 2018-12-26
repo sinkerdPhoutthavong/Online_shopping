9ea0dd1d2162b44d137a2d44bfb929293b66f752
sinkerdPhoutthavong <devn.info@gmail.com>
2018-12-27 00:37:57 +0700

    use relations tables in database in model to show categories in html

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Product;
+use App\Category;
+use function GuzzleHttp\json_encode;
 
 class IndexController extends Controller
 {
@@ -16,6 +18,43 @@ class IndexController extends Controller
 
         //In Random Order
         $productsAll = product::inRandomOrder()->get();
-        return view('index')->with(compact('productsAll'));
+
+        // Get all Categories and sub Categories 
+
+        $categories = Category::with('categories')->where(['parent_id'=>0])->get();
+        // $categories = json_decode(json_encode($categories));
+        // echo "<pre>",print_r($categories);die;
+
+
+        //basic category im html
+       /* $categories_menu = "";
+        foreach ($categories as $cat) {
+            $categories_menu .=" <div class='panel-heading'>
+                                <h4 class='panel-title'>
+                                    <a data-toggle='collapse' data-parent='#accordian'
+                                         href='#".$cat->id."'>
+                                        <span class='badge pull-right'><i class='fa fa-plus'></i></span>
+                                        ".$cat->name."
+                                    </a>
+                                </h4>
+                            </div>
+                           <div id='".$cat->id."' class='panel-collapse collapse'>
+                                <div class='panel-body'>
+                                    <ul>";
+                                    $sub_categories = Category::where(['parent_id'=>$cat->id])
+                                    ->get();
+                                    foreach ($sub_categories as $subcat) {
+                                        $categories_menu .="<li><a href='".$subcat->url."'>
+                                        ".$subcat->name." </a></li>";        
+                                    }                     
+                                        $categories_menu .="</ul>
+                                </div>
+                             </div>
+                            ";
+            
+        }*/
+        //end
+
+        return view('index')->with(compact('productsAll','categories_menu','categories'));
     }
 }
